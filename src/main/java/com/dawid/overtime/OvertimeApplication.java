d2f81a5615c1686c50d69c1190786c6fee1468cf
infarot <infarot@gmail.com>
2019-05-08 11:50:47 +0200

    Initial commit

---
+++ src/main/java/com/dawid/overtime/OvertimeApplication.java
@@ -0,0 +1,13 @@
+package com.dawid.overtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertimeApplication.class, args);
+	}
+
+}

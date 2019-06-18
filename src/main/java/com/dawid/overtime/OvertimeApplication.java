b70db49862cf626a1dfa653aaef245988fedfd71
infarot <infarot@gmail.com>
2019-06-18 11:00:24 +0200

    added eu timezone handling

--- src/main/java/com/dawid/overtime/OvertimeApplication.java
+++ src/main/java/com/dawid/overtime/OvertimeApplication.java
@@ -3,9 +3,16 @@ package com.dawid.overtime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class OvertimeApplication {
 
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Warsaw"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OvertimeApplication.class, args);
 	}
